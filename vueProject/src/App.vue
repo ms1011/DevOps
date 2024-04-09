a7f4db8eae2b159d1d134d2259b5e0dc4f50f345
wkdalstjr94 <wkdalstjr94@gmail.com>
2024-04-09 11:27:48 +0900

    kubernetes와 jenkins 사용해보기

--- vueProject/src/App.vue
+++ vueProject/src/App.vue
@@ -21,7 +21,15 @@ const sendPlus = async() => {
   // const response = await fetch(`http://localhost:5173/api/plus?num1=${num1.value}&num2=${num2.value}`);  // Front에서 CORS 처리
   
   /* 백엔드를 도커 컨테이너로 8055 포트로 만들었을 때 */
-  const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
+  // const response = await fetch(`http://localhost:8055/plus?num1=${num1.value}&num2=${num2.value}`);
+
+  /* 도커 컨테이너 간에 네트워크 연결 후 (:8011/api로 바꿔야 한다.) */
+  // const response = await fetch(`http://localhost:8011/api/plus?num1=${num1.value}&num2=${num2.value}`);
+  
+  /* 백엔드를 쿠퍼네티스를 활용한 워커노드로 띄웠을 경우(30001번 NodePort) */
+  const response = await fetch(`http://localhost:30001/plus?num1=${num1.value}&num2=${num2.value}`);
+
+
   const data = await response.json();
   console.table(data);
   result.value = data.sum;
--- vueProject/vite.config.js
+++ vueProject/vite.config.js
@@ -16,7 +16,14 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7777',
+        // target: 'http://localhost:7777',
+
+        /* 백엔드 프로젝트를 도커 컨테이너(8055:7777)로 바꾼 이후 */
+        // target: 'http://localhost:8055',
+
+        /* 프론트 프로젝트(vue)를 도커 컨테이너(8011:5173)로 바꾸고 도커 네트워크까지 연결 이후 */
+        target: 'http://springboot-app:7777',
+
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
